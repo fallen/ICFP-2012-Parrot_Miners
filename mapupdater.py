bd684cc775db28b238367d67c2a5828ab05b18c2
Luc Banda <luc@luc-bureau.(none)>
2012-07-13 16:15:43 +0200

    start update (3 first statements implemented)

--- mapupdater.py
+++ mapupdater.py
@@ -1,8 +1,28 @@
+import copy
 
 class controller:
 	
 	def __init__(self, lambda_map):
 		self.lambda_map = lambda_map
-
+	
+	def single_round(self):
+		#allocate
+		new_map = copy.deepcopy(self.lambda_map)
+		#set it to ''
+		for x in range(len(new_map)):
+			for y in range(len(new_map[x])):
+				if self.lambda_map[x][y] == '*':
+					if self.lambda_map[x][y-1] == ' ':
+						new_map[x][y] = ' '
+						new_map[x][y-1] = '*'
+					if self.lambda_map[x][y-1] == '*' and self.lambda_map[x+1][y] == ' ' and self.lambda_map[x+1][y-1] == ' ':
+						new_map[x][y] = ' '
+						new_map[x+1][y-1] = '*'
+					if self.lambda_map[x][y-1] == '*' and (self.lambda_map[x+1][y] != ' ' or self.lambda_map[x+1][y-1] != ' ') and self.lambda_map[x-1][y] == ' ' and self.lambda_map[x-1][y-1] == ' ':
+						new_map[x][y] = ' '
+						new_map[x-1][y-1] = ' '
+						
+		self.lambda_map = new_map
+		
 	def set_movement(self, move):
-		pass
+		self.single_round()
